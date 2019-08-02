b7859352dca6633f229851931b3815214acb79b0
Paulo Chaves Filho <pchavesfilho@gmail.com>
2019-08-02 11:04:59 -0300

    Ajuste de arquivos

--- viewscache/footer.fcd02a482a31677de33a0febc7865410.rtpl.php
+++
@@ -1,5 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>
-
-
-</body>
-</html>	
--- viewscache/principal.fcd02a482a31677de33a0febc7865410.rtpl.php
+++
@@ -1,6 +0,0 @@
-<?php if(!class_exists('Rain\Tpl')){exit;}?>
-
-
-	<h1>Hello!</h1>
-	<p>Página principal do site.</p>
-
